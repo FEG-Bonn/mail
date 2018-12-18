7c1d0538ef9bab0b188378c1a74b94532a2b75a7
arthuro555 <arthur.pacaud@hotmail.fr>
2018-12-18 17:25:10 +0100

    Minor database changes.
    Signed-off-by: arthuro555 <arthur.pacaud@gmail.com>

--- mailapi/models.py
+++ mailapi/models.py
@@ -1,1 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
 # Create your models here.
+
+class Mails(models.Model):
+    von = models.EmailField()
+    objekt = models.CharField(max_length=120, blank=True)
+    text = models.TextField(max_length=2000000)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.objekt
