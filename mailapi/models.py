7bf6a11b7ab82ef7d0939665836cf9004253cef1
Arthur <arthur.pacaud@hotmail.fr>
2019-01-08 17:16:18 +0000

    Oh made an error with the conflicts. Actually made the models.py of another project.

--- mailapi/models.py
+++ mailapi/models.py
@@ -1,15 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    text = models.TextField(max_length=1200)
-    background_image = models.FileField(null= True, blank= True, upload_to="user/data/uploads")
-    time = models.fields.DateTimeField(blank = True, null = True, auto_now=True, auto_created=True)
+class Mails(models.Model):
+    von = models.EmailField()
+    objekt = models.CharField(max_length=120, blank=True)
+    text = models.TextField(max_length=2000000)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.title)+", Last editing: "+str(self.time)
+        return self.objekt
---
+++ webvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/arthuro555/homepage/webvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
